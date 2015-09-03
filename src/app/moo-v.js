a53cc7d65a7354a38f6d65f76a383f68019b0770
Rocky Assad <r@cky.email>
2015-09-03 11:00:12 -0500

    add app deps

--- src/app/moo-v.js
+++ src/app/moo-v.js
@@ -1,3 +1,19 @@
 /**
  * Created by fourq on 9/2/15.
  */
+'use strict';
+
+angular.module('moo-v', [
+  'ui.router',
+  'moo-v.common'
+])
+  .config(function ($stateProvider, $urlRouterProvider) {
+
+    $stateProvider
+      .state('movies', {
+        url:'/movies',
+        templateUrl: 'app/movies/movies-mdv.tmpl.html',
+        controller: 'MoviesCtrl',
+        controllerAs: 'ctrl'
+      });
+  });
